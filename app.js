2b2c66691811357337273a6189d0b4bd06193543
Evgeny Karasev <gnomskg@gmail.com>
2020-06-22 00:56:12 +0300

    feat: add getting journal quartile by issn

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
-var express = require("express");
-var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
 
-var indexRouter = require("./routes/index");
+const quartiles = require("./quartiles");
 
-var app = express();
+const indexRouter = require("./routes/index");
+const quartilesRouter = require("./routes/quartiles");
+
+const app = express();
 
 app.use(logger("dev"));
 app.use(express.json());
@@ -13,6 +16,14 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
+app.use(quartiles(path.join(__dirname, "wos_collection.xlsx")));
+
 app.use("/", indexRouter);
+app.use("/quartiles", quartilesRouter);
+
+app.use(function (err, req, res, next) {
+  console.error(err.stack);
+  res.status(500).send("Error");
+});
 
 module.exports = app;
